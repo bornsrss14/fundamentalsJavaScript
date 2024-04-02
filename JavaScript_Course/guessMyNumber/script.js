aa8143b6b1aa982d7684a58f1dfccc3e34d3ff83
bornsrss14 <bornsrss14@gmail.com>
2024-04-02 16:43:48 -0600

    Dom manipulation .querySelector() method

---
+++ JavaScript_Course/guessMyNumber/script.js
@@ -0,0 +1,6 @@
+'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Hello ';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 14;
