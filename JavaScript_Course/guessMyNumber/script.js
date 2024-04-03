4f3102c32cd4793c5ba5d8f6018beaedfd98c8ea
bornsrss14 <bornsrss14@gmail.com>
2024-04-02 23:22:52 -0600

    implementando la logica del juego

--- JavaScript_Course/guessMyNumber/script.js
+++ JavaScript_Course/guessMyNumber/script.js
@@ -1,6 +1,23 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Hello ';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Hello ';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.number').textContent = 14;
 
-document.querySelector('.number').textContent = 14;
+// 1.- Cuando comenzamos el juego, el numero ingresado en el input debe 
+// ser recuperado, comencemos por eso, guardarlo en una variable
+// 2. También debemos de verificar si el valor es valido
+//    2.1 Cuando el numero es correcto
+//    2.2 Cuando el numero es muy alto
+//    2.3 Cuando el numero es muy bajo
+const diceRoll = Math.trunc(Math.random()*20)+1; //give us a number btween 0 n 1 
+document.querySelector('.number').textContent = diceRoll;
+ 
+document.querySelector('.check').addEventListener('click', function(){
+    let guessVariable = +document.querySelector('.guess').value;
+    if(!guessVariable){
+        document.querySelector('.message').textContent = 'ヽ(ー_ー )ノ invalid number';
+    } else if(diceRoll === guessVariable){
+        document.querySelector('.message').textContent = 'シ Correct Number!';
+    }
+});
