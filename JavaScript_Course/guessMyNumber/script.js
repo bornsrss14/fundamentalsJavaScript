cb54de60dcf9df347a04461584748ece979ddd52
bornsrss14 <bornsrss14@gmail.com>
2024-04-04 23:17:22 -0600

    terminado

--- JavaScript_Course/guessMyNumber/script.js
+++ JavaScript_Course/guessMyNumber/script.js
@@ -16,7 +16,15 @@
 let diceRoll = Math.trunc(Math.random() * 20) + 1; //give us a number btween 0 n 1
 let score__inicial = 20;
 const start = "Start guessing...";
+let highScore = 0;
 // document.querySelector(".number").textContent = diceRoll;
+
+let gameLost = function () {
+  document.querySelector(".message").textContent = "You lost the game!";
+  document.querySelector(".score").textContent = 0;
+  document.querySelector(".check").style.display = "none"; // Oculta el botón "Check"
+  document.querySelector(".again").style.display = "block"; // Oculta el botón "Check"
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   let guessVariable = +document.querySelector(".guess").value;
@@ -28,6 +36,10 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".message").textContent = "シ Correct Number!";
     document.querySelector(".number").textContent = diceRoll;
+    if (score__inicial > highScore) {
+      highScore = score__inicial;
+      document.querySelector(".highscore").textContent = highScore;
+    }
     document.querySelector(".check").style.display = "none"; // Oculta el botón "Check"
     document.querySelector(".again").style.display = "block";
   } else if (diceRoll > guessVariable) {
@@ -36,10 +48,7 @@ document.querySelector(".check").addEventListener("click", function () {
       score__inicial--;
       document.querySelector(".score").textContent = score__inicial;
     } else {
-      document.querySelector(".message").textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
-      document.querySelector(".check").style.display = "none"; // Oculta el botón "Check"
-      document.querySelector(".again").style.display = "block"; // Oculta el botón "Check"
+      return gameLost();
     }
   } else if (diceRoll < guessVariable) {
     if (score__inicial > 1) {
@@ -47,10 +56,7 @@ document.querySelector(".check").addEventListener("click", function () {
       score__inicial--;
       document.querySelector(".score").textContent = score__inicial;
     } else {
-      document.querySelector(".message").textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
-      document.querySelector(".check").style.display = "none"; // Oculta el botón "Check"
-      document.querySelector(".again").style.display = "block"; // Oculta el botón "Check"
+      return gameLost();
     }
   }
 });
