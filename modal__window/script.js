b690817c3ce0851e0cef1ff219db473f85d3d734
bornsrss14 <bornsrss14@gmail.com>
2024-04-08 22:46:53 -0600

    todo en orden

--- modal__window/script.js
+++ modal__window/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// const ejemploVariable = documento.querySelector('.claseEjemplo'); selecciona todos los elementos de html que contienen la clase
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
@@ -10,13 +10,20 @@ const closeModalFunc = function () { //para cerrar las ventanas y volver a su es
     overlay.classList.add('hidden');
 };
 
-const openModalFunc = function () {
+const openModalFunc = function () { //para que se muestre el modal al quitar la clase hidden
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener("click", openModalFunc);
 }
-
-btnCloseModal.addEventListener("click", closeModalFunc);
+btnCloseModal.addEventListener("click", closeModalFunc);//agregamos un evento
 overlay.addEventListener('click', closeModalFunc);
+
+// Para manejar eventos de teclado
+document.addEventListener('keydown', function(e){
+    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
+        closeModalFunc();
+    }
+});
