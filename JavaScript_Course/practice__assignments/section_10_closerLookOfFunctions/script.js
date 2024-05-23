d6b95e539e93016afbcc1615b5ed2e305ccb64bf
bornsrss14 <bornsrss14@gmail.com>
2024-05-22 23:49:50 -0600

    Referencias vs valores en objetos, tipos primitivos y objetos como referenciado en funciones

---
+++ JavaScript_Course/practice__assignments/section_10_closerLookOfFunctions/script.js
@@ -0,0 +1,34 @@
+'use strict';
+// const arrBookin = [];
+// const createBooking = function(flightNumber , numPassengers = 1, price =20){
+//     const booking = {
+//         flightNumber,
+//         numPassengers,
+//         price,
+//     };
+//     console.log(booking);
+//     arrBookin.push(booking);
+// };
+// createBooking('SFR14526640', 3);
+// createBooking('SFR44412831', undefined, 84);
+
+const flight = 'HL244';
+const passengerRoss = {
+    name: 'Ross Fuentes',
+    passportNumber : 2475891,
+};
+
+const funChecking = (flightNumber, passenger) =>{
+    flightNumber = 'LH999';
+    passenger.name = 'Ms.' + passenger.name;
+    if(passenger.passportNumber === 2475891 ){
+        console.log(`Checked in ${flightNumber}`);
+    }else{
+        console.log('incorrect ._.');
+    }
+
+};
+
+funChecking(flight, passengerRoss);
+console.log(`Number of flight: ${flight}`);
+console.log(passengerRoss);
