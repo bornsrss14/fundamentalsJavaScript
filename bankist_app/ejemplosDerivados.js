187f6b5d97a2ba75d859ab874c6860a3702c6b7d
bornsrss14 <bornsrss14@gmail.com>
2024-06-19 12:16:22 -0600

    Agregando un timeOut para eliminar el elemento creeado

--- bankist_app/ejemplosDerivados.js
+++ bankist_app/ejemplosDerivados.js
@@ -4,9 +4,10 @@ const funMensaje = function (e) {
   e.preventDefault();
   let div = document.createElement("div"); //que tipo de elemento vamos a crear
   div.className = "alert"; // Cómo se va a ver -(qué clase le corresponde)-
-  div.innerHTML = 
+  div.innerHTML =
     "<strong>¡Hola!</strong> Usted ha leído un importante mensaje."; //Qué es lo que vamos a insertar
-  document.body.append(div); // dónde se va a insertar
+  document.body.prepend(div); // dónde se va a insertar after, before, preppend, append etc.
+  setTimeout(() => div.remove(), 2000);
 };
 btn_login.addEventListener("click", funMensaje);
 //los agrega al final de el elemento seleccionado
