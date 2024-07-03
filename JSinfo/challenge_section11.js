95dcf3f0b7f230b4624836cc1c671c5113c125cb
bornsrss14 <bornsrss14@gmail.com>
2024-07-03 01:47:07 -0600

    Coding Challenge #4: Step 4

--- JSinfo/challenge_section11.js
+++ JSinfo/challenge_section11.js
@@ -80,3 +80,99 @@ funCalcAverageHumanAge(testData2);
 // let numSum = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 // let numeroRed = numSum.reduce((acc, number) => acc + number, 0);
 // console.log(numeroRed);
+
+//TEST DATA
+// Study if each dog are eating to mucho o too little?
+//Comer mucho significa que la porción de comida es más alta que la RECOMENDADA : poc---? lo contrario
+//Iterar sobre el array forEach
+
+//Formula: recFood = peso (KG) ** 0,75 * 28.
+// arryDogs.item.recommendedFood = recFood;
+
+//1. Recorra la matriz que contiene objetos para perros y, para cada perro,
+//calcule la porción de comida recomendada y agréguela al objeto como una nueva propiedad.
+//NO cree una nueva matriz, simplemente recorra la matriz.  Forumla:
+//alimento recomendado = peso ** 0,75 * 28. (El resultado está en gramos de alimento y el peso debe estar en kg)
+//Formula: recFood = peso (KG) ** 0,75 * 28.
+// arryDogs.item.recommendedFood = recFood;
+
+//2. Encuentre el perro de Sarah e inicie sesión en la consola, ya sea que esté comiendo demasiado o muy poco.
+//PISTA: Algunos perros tienen varios dueños, por lo que primero debes encontrar a Sarah en la lista de dueños,
+//por lo que este es un poco complicado (a propósito) 🤓
+
+//3. Cree una matriz que contenga todos los dueños de perros que comen demasiado ('ownersEatTooMuch')
+//y una matriz con todos los dueños de perros que comen muy poco ('ownersEatTooLittle').
+
+//4. Log a string to the console for each array created in 3., like this:
+//"Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+
+//5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended
+//(just true or false)
+
+//6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+
+//7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+
+//8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order
+//(keep in mind that the portions are inside the array's objects)
+
+//PISTA 2: Estar dentro de un rango 10% por encima y por debajo de la porción recomendada
+//significa: actual > (recomendado * 0,90) && actual < (recomendado * 1,10).
+//Básicamente, la porción actual debe estar entre el 90% y el 110% de la porción recomendada.
+
+const dogs = [
+  { name: "Rex", weight: 22, curFood: 250, owners: ["Alice", "Bob"] },
+  { name: "Bella", weight: 8, curFood: 200, owners: ["Matilda"] },
+  { name: "Rocky", weight: 13, curFood: 275, owners: ["Sarah", "Jhon"] },
+  { name: "Max", weight: 32, curFood: 340, owners: ["Michael"] },
+  { name: "Floofy", weight: 14, curFood: 250, owners: ["Ros"] },
+];
+
+console.log(dogs);
+
+//recorre el arreglo de dogs
+const funOne = function (arry) {
+  arry.forEach((dog) => {
+    dog.recommendedFood = Math.trunc(dog.weight ** 0.75 * 28);
+  });
+};
+
+const funTwo = function (arry, owner) {
+  const dogOfPerson = arry.find((dog) => dog.owners.includes(owner));
+  if (dogs) {
+    console.log(
+      `${owner}'s dog ${dogOfPerson.name}, is eating too ${
+        dogOfPerson.curFood > dogOfPerson.recommendedFood ? "much" : "little "
+      }`
+    );
+  }
+};
+
+const funThree = function (dogsArry) {
+  const dogOwnersWithOvereatingDogs = [];
+  const dogOwnersWithUnderfedDogs = [];
+
+  dogsArry.map((dog) =>
+    dog.curFood > dog.recommendedFood
+      ? dogOwnersWithOvereatingDogs.push(dog.owners)
+      : dogOwnersWithUnderfedDogs.push(dog.owners)
+  );
+  console.log(
+    `${dogOwnersWithOvereatingDogs
+      .flat(Infinity)
+      .join(" , ")}'s dogs eat too much! \n${dogOwnersWithUnderfedDogs
+      .flat(Infinity)
+      .join(" , ")}'s dogs eat too little! `
+  );
+  // console.log(...dogOwnersWithOvereatingDogs.flat(Infinity));
+  // console.log(...dogOwnersWithUnderfedDogs.flat(Infinity));
+};
+
+// const allOwners = arry.flatMap((el) => el.owners);
+
+//LLAMADAS DE FUNCIÓN -------------------------------------------------------₊˚🖇️✩ ( ｡•̀ ᴖ •́ ｡)💢 ˚🎧⊹♡
+console.log(funOne(dogs));
+funTwo(dogs, "Michael");
+funTwo(dogs, "Ros");
+
+funThree(dogs);
--- JSinfo/ejemplosBind.js
+++ JSinfo/ejemplosBind.js
@@ -1,140 +1,174 @@
-"use strict";
+// "use strict";
 
-//TENGO UN OBJETO CON UNA PROPIEDAD DE MÉTODO
-// const object = {
-//   initial: 0,
-//   final: 4,
-//   increment: function (step) {
-//     this.final += step;
-//     return this.final;
-//   },
-// };
+// //TENGO UN OBJETO CON UNA PROPIEDAD DE MÉTODO
+// // const object = {
+// //   initial: 0,
+// //   final: 4,
+// //   increment: function (step) {
+// //     this.final += step;
+// //     return this.final;
+// //   },
+// // };
 
-// const object2 = {
-//   initial: 4,
-//   final: 1,
-// };
+// // const object2 = {
+// //   initial: 4,
+// //   final: 1,
+// // };
 
-//accedo a ese metodo con bind y creo un nuevo metodo
-// const copiaIncrementBind = object.increment.bind({final:10});
-// console.log(copiaIncrementBind(50)); //salida de 60
-// const funObjectDosBind = object.increment.bind(object2);
-// console.log(funObjectDosBind(90));
+// //accedo a ese metodo con bind y creo un nuevo metodo
+// // const copiaIncrementBind = object.increment.bind({final:10});
+// // console.log(copiaIncrementBind(50)); //salida de 60
+// // const funObjectDosBind = object.increment.bind(object2);
+// // console.log(funObjectDosBind(90));
 
-//HAY DE DOS OPCIONES, ASÍ YO LO ENTIENDO
-//donde puedo acceder al this(a lo que apunta) en object.nombre de la funcion -> accediendo a bind, y cambiar su propiedad
-//para que a la llamada de la copia que se generó del mètodo, yo pueda acceder a ingresar
-//los argumentos, en este caso step
+// //HAY DE DOS OPCIONES, ASÍ YO LO ENTIENDO
+// //donde puedo acceder al this(a lo que apunta) en object.nombre de la funcion -> accediendo a bind, y cambiar su propiedad
+// //para que a la llamada de la copia que se generó del mètodo, yo pueda acceder a ingresar
+// //los argumentos, en este caso step
 
-// const arr = ['Rosario', 'Fuentes', 'Garcia'];
-// const others = ['is', 'a', 'bitch'];
+// // const arr = ['Rosario', 'Fuentes', 'Garcia'];
+// // const others = ['is', 'a', 'bitch'];
 
-// arr.push.apply(arr, others);
-// for (const iterable of arr) {
-//   console.log(`This is the resoult of: ${iterable}`);
+// // arr.push.apply(arr, others);
+// // for (const iterable of arr) {
+// //   console.log(`This is the resoult of: ${iterable}`);
 
-// }
+// // }
 
-// const numbers = [9, 8, 1,78,6,-22, 2, 3, 5, 6, 7];
+// // const numbers = [9, 8, 1,78,6,-22, 2, 3, 5, 6, 7];
 
-// const max = Math.max.apply(null, numbers);
-// const min = Math.min.apply(null, numbers);
+// // const max = Math.max.apply(null, numbers);
+// // const min = Math.min.apply(null, numbers);
 
-// console.log(max);
-// console.log(min);
+// // console.log(max);
+// // console.log(min);
 
-// (function(){
-//   console.log('this will never executed again');
-// })();
+// // (function(){
+// //   console.log('this will never executed again');
+// // })();
 
-// (() => console.log('Esto no se volverá a repetir'))();
+// // (() => console.log('Esto no se volverá a repetir'))();
 
-let f;
+// // let f;
 
-const g = function () {
-  const a = 23;
+// // const g = function () {
+// //   const a = 23;
 
-  f = function () {
-    console.log(a * 2);
-  };
-};
+// //   f = function () {
+// //     console.log(a * 2);
+// //   };
+// // };
 
-const h = function () {
-  const b = 14;
-  f = function () {
-    console.log(b * 2);
-  };
-};
+// // const h = function () {
+// //   const b = 14;
+// //   f = function () {
+// //     console.log(b * 2);
+// //   };
+// // };
 
-//the first adignment
-g();
-f();
+// //the first adignment
+// // g();
+// // f();
 
-//f es una otra funcion, porque fue reasignada por h en la segunda funcion
-//esto prueba que f. se cerró sobre el entorno variable de h()
-h();
-f();
+// //f es una otra funcion, porque fue reasignada por h en la segunda funcion
+// //esto prueba que f. se cerró sobre el entorno variable de h()
+// // h();
+// // f();
 
-/////////////////// creando otra función
+// /////////////////// creando otra función
 
-const funEspera = function (numPass, waitTime) {
-  const perGroup = numPass / 3;
+// // const funEspera = function (numPass, waitTime) {
+// //   const perGroup = numPass / 3;
 
-  setTimeout(function () {
-    console.log(
-      `Now we are boarding all ${numPass} passengers  in ${waitTime} seconds`
-    );
-    console.log(
-      `There are 3 groups , each with ${perGroup} passengers per group`
-    );
-  }, waitTime * 1000);
+// //   setTimeout(function () {
+// //     console.log(
+// //       `Now we are boarding all ${numPass} passengers  in ${waitTime} seconds`
+// //     );
+// //     console.log(
+// //       `There are 3 groups , each with ${perGroup} passengers per group`
+// //     );
+// //   }, waitTime * 1000);
 
-  console.log(`Will start boarding in ${waitTime} seconds`);
-};
+// //   console.log(`Will start boarding in ${waitTime} seconds`);
+// // };
 
-funEspera(18, 14);
+// // funEspera(18, 14);
 
-//-------------------------------------------------------------------------------
-(function () {
-  const acheUno = document.querySelector("h1");
-  acheUno.style.color = "red";
+// //-------------------------------------------------------------------------------
+// (function () {
+//   const acheUno = document.querySelector("h1");
+//   acheUno.style.color = "red";
 
-  document.querySelector("body").addEventListener("click", function () {
-    acheUno.style.color = "steelblue";
-  });
-})(); //auto ejecutable para evitar dejar variabales en el ambito global
+//   document.querySelector("body").addEventListener("click", function () {
+//     acheUno.style.color = "steelblue";
+//   });
+// })(); //auto ejecutable para evitar dejar variabales en el ambito global
 
-// The new arrays methods
-// .concat()    .join()    .reverse()   .slice()
+// // The new arrays methods
+// // .concat()    .join()    .reverse()   .slice()
 
-const arySingles = [
-  "Turn on the bright lights",
-  "stella was a diver and she was allways down",
-  "obstacle 1",
-  "Barricade",
-  "No I in threesome",
-  "Rest my Chemistry",
-];
+// // const arySingles = [
+// //   "Turn on the bright lights",
+// //   "stella was a diver and she was allways down",
+// //   "obstacle 1",
+// //   "Barricade",
+// //   "No I in threesome",
+// //   "Rest my Chemistry",
+// // ];
 
-const arySinglesTwo = ["All the  rages back home", "Evil", "NYK", "Toni"];
+// // const arySinglesTwo = ["All the  rages back home", "Evil", "NYK", "Toni"];
 
-const concat1= arySingles.concat(arySinglesTwo);//copia superficial
-console.log(...concat1.join(''));
+// // const concat1 = arySingles.concat(arySinglesTwo); //copia superficial
+// // console.log(...concat1.join(""));
 
-////// Looping Arrays forEach()
+// ////// Looping Arrays forEach()
 
-const arryMovements = [467,-56,2000,-800,-25,-3,-56,892,5000];
-//large form
-for (const movement of arryMovements) {
-  if(movement > 0){
-    console.log(`Usted hizo un deposito de ${movement}`);
-  } else{
-    console.log(`Usted hizo un retiro de ${Math.abs(movement)}`);
-  }
-}
-console.log(arryMovements);
+// // const arryMovements = [467, -56, 2000, -800, -25, -3, -56, 892, 5000];
+// // //large form
+// // for (const [indi, movement] of arryMovements.entries()) {
+// //   if (movement > 0) {
+// //     console.log(`Usted hizo un deposito de: ${indi + 1}.- ${movement}`);
+// //   } else {
+// //     console.log(`Usted hizo un retiro de: ${indi + 1}.- ${Math.abs(movement)}`);
+// //   }
+// // }
+// // console.log(arryMovements);
 
-//Short form
-for (const mov of arryMovements) {
-  const result = mov > 0 ? console.log(`Deposito de ${mov}`) : console.log(`retiro de ${Math.abs(mov)}`);
-}
+// //Short form
+// // for (const mov of arryMovements) {
+// //   const result =
+// //     mov > 0
+// //       ? console.log(`Deposito de ${mov}`)
+// //       : console.log(`retiro de ${Math.abs(mov)}`);
+// // }
+
+// //With for EachMethod
+// console.log(
+//   "--------------- con parametros de indice, replicando a .entries() method"
+// );
+
+// // arryMovements.forEach(function (movement, indi, array) {
+// //   //què pasa si necesitamos acceso a una variable de contador(?)
+// //   movement > 0
+// //     ? console.log(`Deposito ${indi + 1}: de ${movement}`)
+// //     : console.log(`Retiro de ${indi + 1}: de ${Math.abs(movement)}`);
+// // });
+
+// // console.log(
+// //   "Con función de flecha =>------------ imitando al .entries() method--------------"
+// // );
+// // arryMovements.forEach((movement, index, array) =>
+// //   movement > 0
+// //     ? console.log(`Deposito ${index + 1}: de ${movement}`)
+// //     : console.log(`Retiro ${index + 1}: de ${Math.abs(movement)}`)
+// // );
+
+// //--- El forEach también funciona en Maps y Sets
+// //MAPS mis pares key, value
+
+// const monedas = [
+//   ["USD", "Dollar"],
+//   ["MXN", "Pesos"],
+//   ["IND", "Rupia"],
+//   ["EUR", "Londres"],
+// ];
