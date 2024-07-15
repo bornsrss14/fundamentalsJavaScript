47b7e61f2b24a79754cbfe2b6479e1ce1c20579e
bornsrss14 <bornsrss14@gmail.com>
2024-07-14 23:08:51 -0600

    maquetado

---
+++ Section 13/_web_site/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const btn_sus = document.querySelector(".btn_suscribirme");
+
+/* evitar la carga del form */
+btn_sus.addEventListener("click", function (e) {
+  e.preventDefault();
+});
