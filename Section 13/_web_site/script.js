54e864a64ebfd2653d07898a3310c288c8601304
bornsrss14 <bornsrss14@gmail.com>
2024-07-17 23:54:29 -0600

    just fixed the header and the distribution, html is done

--- Section 13/_web_site/script.js
+++ Section 13/_web_site/script.js
@@ -5,3 +5,27 @@ const btn_sus = document.querySelector(".btn_suscribirme");
 btn_sus.addEventListener("click", function (e) {
   e.preventDefault();
 });
+/* Showing date at the bottom in the <small> tag */
+const showYear = document.getElementById("showYear");
+showYear.innerHTML = new Date().getFullYear();
+
+/* Button to the navigate easily to top of the page */
+// Obtener el botón
+const toTopBtn = document.getElementById("toTopBtn");
+
+// Mostrar el botón cuando se desplaza hacia abajo 100px desde la parte superior de la página
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    toTopBtn.style.display = "block";
+  } else {
+    toTopBtn.style.display = "none";
+  }
+};
+
+// Cuando se hace clic en el botón, se desplaza hacia arriba de la página
+toTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
