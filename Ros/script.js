72964f38285d54225e82d3f82d01bcf7dc202afe
bornsrss14 <bornsrss14@gmail.com>
2024-07-23 20:29:39 -0600

    Trying with some practices understanding some concepts about events handlers and walking throught the DOM

---
+++ Ros/script.js
@@ -0,0 +1,19 @@
+"use strict";
+/* UNDERTANDING HOW DOM CONTROL THE EVENTS: AND UNDERSTANDING SOME CONCEPTS ABOUT IT, BUBBLING, PROPAGATION, CAPTURING*/
+// let variableTrue = true;
+// const alerta = document.querySelector(".evento_alerta");
+// alerta.addEventListener("click", () => {
+//   alert("First handler");
+// });
+// alerta.addEventListener("click", (event) => {
+// //   event.stopImmediatePropagation(); // Detiene la propagación inmediata
+//   alert("Second handler");
+// });
+// alerta.addEventListener("click", (event) => {
+//     event.stopImmediatePropagation();
+//   alert("Third handler");
+// }, variableTrue);
+// document.querySelector(".evento_cuadroRojo").addEventListener("click", () => {
+//   alert("Parent clicked");
+// });
+
