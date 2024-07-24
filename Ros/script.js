85467b3a878ed1c301c7ec8ab7dc07b2fae0a6aa
bornsrss14 <bornsrss14@gmail.com>
2024-07-23 22:02:35 -0600

    194. DOM Traversing, parent, child, sibblings, children elements manipulation

--- Ros/script.js
+++ Ros/script.js
@@ -16,4 +16,26 @@
 // document.querySelector(".evento_cuadroRojo").addEventListener("click", () => {
 //   alert("Parent clicked");
 // });
+const h1 = document.querySelector('h1');
+console.log(h1.querySelectorAll('.highlight'));
+h1.firstElementChild.style.color = "blue";
+h1.lastElementChild.style.color = 'red';
 
+console.log(`Este es el elemento padre ${h1.parentElement}`);
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach((el) => {
+  if(el !== h1){
+    el.style.transform = 'scale(.7)';
+
+  }
+});
+
+
+const childrenAll = h1.parentElement.children;
+for (let i = 0; i < childrenAll.length; i++) {
+  if(childrenAll[i] !== h1) {
+    childrenAll[i].style.color = "red";
+  }
+  
+}
