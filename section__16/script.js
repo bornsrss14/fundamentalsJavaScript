55f85c92f1b0487dcbef2e45881cb8ecc213a73e
bornsrss14 <bornsrss14@gmail.com>
2024-10-05 23:38:49 -0600

    Update project files: html cards structure and style has completed

--- section__16/script.js
+++ section__16/script.js
@@ -1,7 +1,21 @@
 "use stric";
 
-const img = document.querySelector(".image");
-img.src = "gatito.jpg"; // Set the image source
-img.addEventListener("load", function () {
-  img.classList.add("fadeIn");
-});
+// const img = document.querySelector(".image");
+// img.src = "gatito.jpg"; // Set the image source
+// img.addEventListener("load", function () {
+//   img.classList.add("fadeIn");
+// });
+
+/* EVERYTHING RELATED OF COUNTRIES CARDS */
+function setDisplayFlex() {
+  const parentDivReference = document.querySelector(".divCountryDescription");
+  const childDivs = parentDivReference.querySelectorAll("div");
+  /* Apply display flex at every item */
+  childDivs.forEach((div) => {
+    // div.style.display = "flex";
+    div.classList.add("descriprionCardStyle");
+  });
+}
+
+/* LLAMADA DE FUNCIONES */
+setDisplayFlex();
